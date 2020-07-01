4e1b062c8ca3779c789fbfe7b6e29792c62e0991
Akshay Vishwakarma <67325419+Akshayvishwakarma1406@users.noreply.github.com>
2020-07-01 20:36:35 +0530

    Add files via upload

---
+++ rects.js
@@ -0,0 +1,16 @@
+class Rect {
+  constructor(x, y, width, height) {
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+    fill(255);
+    rect(0, 0, this.width, this.height);
+  }
+}
